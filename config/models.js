6be11adb202836a2d2861c016ba08f2da89ce104
ysw <yswai1986@gmail.com>
2016-02-21 12:43:45 +0800

    login page

--- config/models.js
+++ config/models.js
@@ -18,7 +18,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
   // connection: 'localDiskDb',
-  connection: 'myMongodbServer'
+  connection: 'myMongodbServer',
 
   /***************************************************************************
   *                                                                          *
@@ -28,6 +28,6 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+  migrate: 'alter'
 
 };
